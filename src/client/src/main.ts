bf72fcb0f796738ae8a2346c0683c2a309db71a3
Ilya Lovriakov <ilya.lovriakov@booking.com>
2021-02-12 13:38:07 +0100

    Update dependencies WIP

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -5,14 +5,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import { Promised } from "vue-promised";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 Vue.use(VModal);
-
-Vue.component("Promised", Promised);
 
 new Vue({
   router,
